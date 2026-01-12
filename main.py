12fec40c0c41bebdb0338d13bc8661d33befff62
Bohdan Redchuk <bogdanredchuk0332@gmail.com>
2026-01-12 12:53:55 +0200

    Updates

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import json
 import asyncio
 from telegram import Bot
 from datetime import date, timedelta
@@ -14,15 +15,24 @@ UA_WEEKDAYS = {
     6: "Неділя",
 }
 
-def next_week_wed_thu(today=None):
+def next_week_wed_thu(today: date | None = None):
     if today is None:
         today = date.today()
 
-    # Понеділок наступного тижня
-    next_monday = today + timedelta(days=(7 - today.weekday()))
+    # weekday(): Monday=0 ... Sunday=6
+    WEDNESDAY = 2
+    THURSDAY = 3
 
-    wednesday = next_monday + timedelta(days=2)
-    thursday  = next_monday + timedelta(days=3)
+    days_until_wed = (WEDNESDAY - today.weekday()) % 7
+    days_until_thu = (THURSDAY - today.weekday()) % 7
+
+    # Якщо сьогодні середа або пізніше — беремо наступний тиждень
+    if today.weekday() >= WEDNESDAY:
+        days_until_wed += 7
+        days_until_thu += 7
+
+    wednesday = today + timedelta(days=days_until_wed)
+    thursday = today + timedelta(days=days_until_thu)
 
     wed_str = f"{UA_WEEKDAYS[wednesday.weekday()]}, {wednesday.strftime('%d.%m')}"
     thu_str = f"{UA_WEEKDAYS[thursday.weekday()]}, {thursday.strftime('%d.%m')}"
@@ -49,47 +59,48 @@ def update_table(df_old, df_new):
 
   return df_clean
 
-# TOKEN = '7770236578:AAGrkL_bDEq9N6NLsKYTePL8Ac6XglN4t10'
 TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
 
 CHAT_ID = '-1002643965663'
 THREAD_ID = '16'
 
-# Параметри опитування
-QUESTION_W = ", 20:00, чисто тренування, Ерідон"
-QUESTION_T = ", 20:00, тренування/спаринг, Ерідон"
-OPTIONS = ["󠀼✅👟", "󠀼✅🧤", "󠀭️❌", "🧠"]
-
 wed, thu = next_week_wed_thu(today=None)
 
-QUESTION = wed + QUESTION_W
+# Параметри опитування
+QUESTION_W = wed + ", 20:00, чисто тренування, Ерідон"
+QUESTION_T = thu + ", 20:00, тренування/спаринг, Ерідон"
+OPTIONS = ["󠀼✅👟", "󠀼✅🧤", "󠀭️❌", "🧠"]
 
 async def main():
     bot = Bot(token=TOKEN)
     
     # Надсилаємо опитування
-    await bot.send_poll(
-        chat_id=CHAT_ID,
-        question=QUESTION,
-        options=OPTIONS,
-        is_anonymous=False,      # Щоб ви могли бачити результати
-        allows_multiple_answers=False
-    )
-    print("Опитування надіслано успішно!")
+    w_poll_message = await bot.send_poll(
+                    chat_id=CHAT_ID,
+                    message_thread_id=THREAD_ID,
+                    question=QUESTION_W,
+                    options=OPTIONS,
+                    is_anonymous=False,      # Щоб ви могли бачити результати
+                    allows_multiple_answers=False)
+
+    # Надсилаємо опитування
+    t_poll_message = await bot.send_poll(
+                    chat_id=CHAT_ID,
+                    message_thread_id=THREAD_ID,
+                    question=QUESTION_T,
+                    options=OPTIONS,
+                    is_anonymous=False,      # Щоб ви могли бачити результати
+                    allows_multiple_answers=False)
+
+    # Формуємо дані для збереження
+    data_to_save = { wed:{"message_id": w_poll_message.message_id,
+                          "options": OPTIONS},
+                     thu:{"message_id": t_poll_message.message_id,
+                          "options": OPTIONS},}
+
+    # Записуємо у файл
+    with open('poll_data.json', 'w', encoding='utf-8') as f:
+        json.dump(data_to_save, f, ensure_ascii=False, indent=4)
 
 if __name__ == "__main__":
     asyncio.run(main())
-
-# poll_message = await bot.send_poll(
-#                 chat_id=CHAT_ID,
-#                 message_thread_id=THREAD_ID,
-#                 question=QUESTION,
-#                 options=OPTIONS,
-#                 is_anonymous=False)
-
-# poll_id = poll_message.poll.id
-
-# POLL_META = {"poll_id": poll_id,
-#              "date": wed,
-#              "mesg_id": poll_message.message_id,
-#              "options": OPTIONS}
